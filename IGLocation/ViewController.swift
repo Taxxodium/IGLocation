32a55a54c43414002c1cefb1ec216b4a3452ae33
Jesus De Meyer <jesus@edotstudios.com>
2018-08-14 16:40:24 -0500

    Initial Commit

---
+++ IGLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IGLocation
+//
+//  Created by Jesus De Meyer on 14/8/18.
+//  Copyright © 2018 e dot studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
